7a192d05f47438da299a6a302ebdbfc61998ca1b
Jordan-B1 <bankolejordan75@gmail.com>
2024-03-24 22:52:56 +0100

    feat(polymorphism) revers shell is working

--- Polymorphic/Linux-ELF/src/ELF/find_section.c
+++ Polymorphic/Linux-ELF/src/ELF/find_section.c
@@ -1,26 +1,21 @@
-/*
-** EPITECH PROJECT, 2020
-** Whitecomet-Research
-** File description:
-** find_section
-*/
-#include <string.h>
-#include <stddef.h>
-#include <elf.h>
 
-Elf64_Shdr *elfi_find_section(void *data, char *name)
-{
+#include <elf.h>
+#include <stddef.h>
+#include <string.h>
+
+Elf64_Shdr *elfi_find_section(void *data, char *name) {
   char *sname;
   int i;
-  Elf64_Ehdr* elf_hdr = (Elf64_Ehdr *) data; /* Get bin info */
+  Elf64_Ehdr *elf_hdr = (Elf64_Ehdr *)data;                   /* Get bin info */
   Elf64_Shdr *shdr = (Elf64_Shdr *)(data + elf_hdr->e_shoff); /* Get sections */
   Elf64_Shdr *sh_strtab = &shdr[elf_hdr->e_shstrndx]; /* Get sections names */
-  const char *const sh_strtab_p = data + sh_strtab->sh_offset; /* Get sections names (pointer) */
+  const char *const sh_strtab_p =
+      data + sh_strtab->sh_offset; /* Get sections names (pointer) */
 
   for (i = 0; i < elf_hdr->e_shnum; i++) {
-      sname = (char *)(sh_strtab_p + shdr[i].sh_name); /* Find sections "name" */
-      if (!strcmp (sname, name))
-        return &shdr[i];
-    }
+    sname = (char *)(sh_strtab_p + shdr[i].sh_name); /* Find sections "name" */
+    if (!strcmp(sname, name))
+      return &shdr[i];
+  }
   return NULL;
 }
